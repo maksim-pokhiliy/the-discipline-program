dc6adb9e64e1275d4a8f6c3b5a30f7dc318b213e
Maksim Pokhiliy <maksim.pokhiliy@gmail.com>
2025-09-19 16:19:48 +0300

    feat: admin auth feature added

--- apps/admin/src/app/layout.tsx
+++ apps/admin/src/app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 };
 
 type RootLayoutProps = Readonly<{
-  children: React.ReactElement;
+  children: React.ReactNode;
 }>;
 
 export default function RootLayout({ children }: RootLayoutProps) {
