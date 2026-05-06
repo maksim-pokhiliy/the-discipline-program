d0067d895ddc0c665d891258de6938eb45500043
Maksim Pokhiliy <maksim.pokhiliy@gmail.com>
2026-05-06 10:33:12 +0300

    feat(api-server): wire plan-content + plan-enrollment into lms barrel
    New plan-content barrel re-exports the four child entity barrels;
    the lms barrel re-exports plan-content and plan-enrollment alongside
    the existing library and training-plan groups.

--- packages/api-server/src/endpoints/lms/index.ts
+++ packages/api-server/src/endpoints/lms/index.ts
@@ -1,2 +1,4 @@
 export * from "./library";
+export * from "./plan-content";
+export * from "./plan-enrollment";
 export * from "./training-plan";
---
+++ packages/api-server/src/endpoints/lms/plan-content/index.ts
@@ -0,0 +1,4 @@
+export * from "./plan-day";
+export * from "./plan-session";
+export * from "./plan-block";
+export * from "./plan-item";
