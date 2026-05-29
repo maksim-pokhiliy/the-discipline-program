b7e9898c73b77a00325e52808d201c22259ba665
Maksim Pokhiliy <maksim.pokhiliy@gmail.com>
2026-05-29 08:26:10 +0300

    feat(ui): add form-section and extend toggle-section and base-modal

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -10,6 +10,7 @@ export * from "./drawer";
 export * from "./dynamic-list-item";
 export * from "./empty-state";
 export * from "./form-pill";
+export * from "./form-section";
 export * from "./form-view";
 export * from "./image-upload";
 export * from "./indicator-chip";
