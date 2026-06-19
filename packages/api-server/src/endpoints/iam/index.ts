229ede7d1d9f6ec4c3c5ec6dd03244e287d00101
Maksim Pokhiliy <maksim.pokhiliy@gmail.com>
2026-06-19 13:17:50 +0300

    feat: add password-reset api service

--- packages/api-server/src/endpoints/iam/index.ts
+++ packages/api-server/src/endpoints/iam/index.ts
@@ -1,6 +1,7 @@
 export * from "./admin-coach-list";
 export * from "./auth-service";
 export * from "./invite-token";
+export * from "./password-reset";
 export * from "./user-creation";
 export * from "./users-admin";
 export * from "./users-search";
